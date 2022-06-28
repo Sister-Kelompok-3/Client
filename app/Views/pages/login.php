0f25e2dc74aae4ac7449a5b50401688750af79e2
Intanadista <86050745+Intanadista@users.noreply.github.com>
2022-06-28 00:29:10 -0700

    upload form login

---
+++ app/Views/pages/login.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+</head>
+
+<body class="bg-light">
+
+<div class="card">
+  <article class="card-body">
+  <a href="" class="float-right btn btn-outline-primary">Sign up</a>
+  <h4 class="card-title mb-4 mt-1">Sign in</h4>
+     <form>
+      <div class="form-group">
+        <label>Your email</label>
+          <input name="" class="form-control" placeholder="Email" type="email">
+      </div> <!-- form-group// -->
+      <div class="form-group">
+        <a class="float-right" href="#">Forgot?</a>
+        <label>Your password</label>
+          <input class="form-control" placeholder="***" type="password">
+      </div> <!-- form-group// --> 
+      <div class="form-group"> 
+      <div class="checkbox">
+        <label> <input type="checkbox"> Save password </label>
+      </div> <!-- checkbox .// -->
+      </div> <!-- form-group// -->  
+      <div class="form-group">
+          
+      </div> <!-- form-group// -->                                                           
+  </form>
+  </article>
+  </div> <!-- card.// -->
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
